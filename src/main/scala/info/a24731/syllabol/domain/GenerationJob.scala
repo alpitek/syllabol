b78fd5f6d723fe36396a99a0551aa01073ad47d0
unbekannt <a24731@gmail.com>
2026-09-19 14:44:40 +0300

    minimal domain of grammar generation

---
+++ src/main/scala/info/a24731/syllabol/domain/GenerationJob.scala
@@ -0,0 +1,8 @@
+package info.a24731.syllabol.domain
+
+import java.util.UUID
+
+opaque type JobId = UUID
+object JobId:
+  def generate(): JobId = UUID.randomUUID()
+  def apply(uuid: UUID): JobId = uuid
---
+++ src/main/scala/info/a24731/syllabol/domain/Grammar.scala
@@ -0,0 +1,3 @@
+package info.a24731.syllabol.domain
+
+case class Grammar(syllablePattern: String, wordPattern: String)
